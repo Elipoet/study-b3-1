31857eb7724e78a3c94218a3ebde933e5c97f241
root <theophile.poumeyrol@gmail.com>
2023-11-15 23:35:39 +0100

    ajout templates base & navbar

--- hello.py
+++ hello.py
@@ -8,8 +8,8 @@ def index():
     return render_template("index.html")
 
 @app.route('/user/<name>')
-def user(name):
-    return render_template("user.html",
+def admin(name):
+    return render_template("admin.html",
                             name=name)
 
 # Pages Invalides
