9ff39aa75c3822ca5b979e975fda54446318ac07
root <theophile.poumeyrol@gmail.com>
2023-11-16 00:37:24 +0100

    tests class et form

--- hello.py
+++ hello.py
@@ -1,6 +1,19 @@
 from flask import Flask, render_template
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField, EmailField, PasswordField
+from wtforms.validators import DataRequired
 
+# creation flask instance
 app = Flask(__name__)
+# creation secret key
+app.config['SECRET_KEY'] = "secretkey_studi_b3"
+
+
+# creation form class
+class NamerForm(FlaskForm):
+    name = StringField("Nom de l'Utilisateur", validators=[DataRequired()])
+    submit = SubmitField("Soumettre")
+
 
 #Accueil'
 @app.route('/')
@@ -21,3 +34,19 @@ def page_not_found(e):
 @app.errorhandler(500)
 def page_not_found(e):
     return render_template("500.html"), 500
+
+# Creation Page Utilisateurs
+@app.route('/users', methods=['GET', 'POST'])
+def users():
+    name = None
+    form = NamerForm()
+    if form.validate_on_submit():
+        name = form.name.data
+        form.name.data = ''
+
+    return render_template("users.html",
+                           name = name, 
+                           form = form)
+
+if __name__ == '__main__':
+    app.run(debug=True)
