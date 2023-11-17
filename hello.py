9de3eb90a5f77ae6d5844c5b10758c743610d171
root <theophile.poumeyrol@gmail.com>
2023-11-17 02:19:05 +0100

    password_hash

--- hello.py
+++ hello.py
@@ -1,9 +1,11 @@
 from flask import Flask, render_template, flash, request
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, EmailField, PasswordField
-from wtforms.validators import DataRequired
+from wtforms import StringField, SubmitField, EmailField, PasswordField, BooleanField, ValidationError
+from wtforms.validators import DataRequired, EqualTo, Length
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 from datetime import datetime
+from werkzeug.security import generate_password_hash, check_password_hash
 
 # creation flask instance
 app = Flask(__name__)
@@ -17,31 +19,50 @@ app.config['SECRET_KEY'] = "secretkey_studi_b3"
 
 #Initialisation db
 db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+# creation form class
+class PasswordForm(FlaskForm):
+    email = StringField("Email", validators=[DataRequired()])
+    password_hash = PasswordField("Mot de Passe", validators=[DataRequired()])
+    submit = SubmitField("Soumettre")
 
 # creation form class
 class NamerForm(FlaskForm):
     name = StringField("Nom de l'Utilisateur", validators=[DataRequired()])
     submit = SubmitField("Soumettre")
 
+# creation form user
+class UserForm(FlaskForm):
+    name = StringField("Nom de l'Utilisateur", validators=[DataRequired()])
+    email = EmailField("Email", validators=[DataRequired()])
+    password_hash = PasswordField("Mot de Passe", validators=[DataRequired(), EqualTo('password_hash2', message="Les mots de passes doivent correspondre")])
+    password_hash2 = PasswordField("Confirmer Mot de Passe", validators=[DataRequired()])
+    submit = SubmitField("Soumettre")
 
 # Modele db
 class Users(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), nullable=False)
     email = db.Column(db.String(100), nullable=False, unique=True)
-    # password = db.Column(db.String(100), nullable=False)
     date_added = db.Column(db.DateTime, default=datetime.utcnow)
+    password_hash = db.Column(db.String(300), nullable=False)
+
+    @property
+    def password(self):
+        raise AttributeError("Le mot de passe n'est pas une valeur visible")
+    
+    @password.setter
+    def password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def verify_password(self, password):
+        return check_password_hash(self.password_hash, password)
 
     # Creation String
     def __repr_(self):
         return '<Name %r>' % self.name
     
-# creation form user
-class UserForm(FlaskForm):
-    name = StringField("Nom de l'Utilisateur", validators=[DataRequired()])
-    email = EmailField("Email", validators=[DataRequired()])
-    submit = SubmitField("Soumettre")
-
 #Accueil'
 @app.route('/')
 def index():
@@ -82,12 +103,14 @@ def ajout():
     if form.validate_on_submit():
         user = Users.query.filter_by(email=form.email.data).first()
         if user is None:
-            user = Users(name=form.name.data, email=form.email.data)
+            hashed_password = generate_password_hash(form.password_hash.data, method='pbkdf2')
+            user = Users(name=form.name.data, email=form.email.data, password_hash=hashed_password)
             db.session.add(user)
             db.session.commit()
         name = form.name.data
         form.name.data = ''
         form.email.data = ''
+        form.password_hash.data = ''
         flash("Utilisateur ajouté")
     our_users = Users.query.order_by(Users.date_added)
     return render_template("ajout_utilisateur.html", 
@@ -103,6 +126,7 @@ def update(id):
     if request.method == "POST":
         name_to_update.name = request.form['name']
         name_to_update.email = request.form['email']
+        name_to_update.password_hash = request.form['password_hash']
         try:
             db.session.commit()
             flash("Mise à Jours Utilisateur réussie")
@@ -117,7 +141,55 @@ def update(id):
     else:
         return render_template("update.html",
                                 form = form,
-                                name_to_update = name_to_update)
+                                name_to_update = name_to_update,
+                                id = id)
+
+@app.route('/supprimer/<int:id>')
+def delete(id):
+    user_to_delete = Users.query.get_or_404(id)
+    name = None
+    form = UserForm()
+
+    try:
+        db.session.delete(user_to_delete)
+        db.session.commit()
+        flash("Suppression Utilisation réussie")
+        our_users = Users.query.order_by(Users.date_added)
+        return render_template("ajout_utilisateur.html", 
+                            name = name,
+                            form = form, 
+                            our_users = our_users)
+    except:
+        flash("La supression de l'utilisateur n'a pas fonctionné")
+        return render_template("ajout_utilisateur.html", 
+                            name = name,
+                            form = form, 
+                            our_users = our_users)
+
+@app.route('/test_pwd', methods=['GET','POST'])
+def test_pwd():
+    email = None
+    password = None
+    pw_to_check = None
+    passed = None
+    form = PasswordForm()
+    if form.validate_on_submit():
+        email = form.email.data
+        password = form.password_hash.data
+        form.email.data = ''
+        form.password_hash.data = ''
+
+        pw_to_check = Users.query.filter_by(email=email).first()
+
+        passed = check_password_hash(pw_to_check.password_hash, password)
+
+    return render_template("test_pwd.html",
+                           email = email,
+                           password = password,
+                           pw_to_check = pw_to_check,
+                           passed = passed, 
+                           form = form)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
